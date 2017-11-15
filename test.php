e50d5557e56a7e8b108416d3fefad6350c9ec25f
Nathanael d. Noblet <nathanael@noblet.ca>
2017-11-15 10:46:37 -0700

    output as text

--- test.php
+++ test.php
@@ -6,7 +6,6 @@ if (!class_exists('\Pdf\PdfText')) {
 
 if (class_exists('\Pdf\PdfWriter')) {
     $pdfWriter = new \Pdf\PdfWriter('./File.pdf','./Output.pdf');
-    echo "Class exists!\n";
     $pdfWriter->writeTextToPage(0, [ new \Pdf\PdfText(78.5,640,"The text to write"),new \Pdf\PdfText(78.5,540,"More text to write"), new \Pdf\PdfText(78.5,440,"Gnat defaced this PDF!")]);
     $pdfWriter->writeTextToPage(1, [ new \Pdf\PdfText(98.5,640,"The text to write"),new \Pdf\PdfText(98.5,540,"More text to write"), new \Pdf\PdfText(98.5,440,"Gnat defaced this PDF!")]);
     $pdfWriter->save();
@@ -18,13 +17,17 @@ if (class_exists('\Pdf\PdfDocument')) {
     //$pdfDocument = new \Pdf\PdfDocument('./File.pdf');
     $pdfDoc = new \Pdf\PdfDocument('./File.pdf','user','password');
 
-    echo "Class exists!\n";
-
     echo "PDF Version: ".$pdfDoc->getMajorVersion().'.'.$pdfDoc->getMinorVersion()."\n";
     echo "Is Encrypted: ".(($pdfDoc->isEncrypted() === true)?'Yes':'No')."\n";
     echo "Is Linear: ".(($pdfDoc->isLinear() === true)?'Yes':'No')."\n";
     echo "NumPages: ".$pdfDoc->getNumberOfPages()."\n";
 
+    $txt = $pdfDoc->asString();
+
+    echo "TXT\n$txt\n\n";
+
+    echo "ORD: ".ord($txt)."\n";
+    file_put_contents('./Output.txt',$pdfDoc->asString());
     $path = "/home/gnat/Projects/Web/prolegis/web/Output";
     echo "Outputting as jpg...\n";
     $res = $pdfDoc->toImage(\Pdf\PdfDocument::IMAGE_JPEG,$path,300);
